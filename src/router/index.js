909de1ce5174912401d3cb7d8be1749e82a07966
vopham <phamchauvo219112001@gmail.com>
2023-03-26 17:16:13 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,15 @@ import ContactBook from "@/views/ContactBook.vue";
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
     ];
     const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
         routes,
     });
+
 export default router;
